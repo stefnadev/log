5028e0c795a82d7a62fd300b03091089f407d207
Andreas Sundqvist <andreas@stefna.is>
2019-01-22 09:27:31 +0100

    Add buffer logger
    This can be used with activate filter to create the crash logger
    So when the an error happens all log entries will be logged to the log
    file

---
+++ src/Logger/BufferLogger.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types=1);
+
+namespace Stefna\Logger\Logger;
+
+use Psr\Log\AbstractLogger;
+
+class BufferLogger extends AbstractLogger
+{
+	protected $buffer = [];
+
+	/**
+	 * Logs with an arbitrary level.
+	 *
+	 * @param mixed $level
+	 * @param string $message
+	 * @param array $context
+	 *
+	 * @return void
+	 */
+	public function log($level, $message, array $context = [])
+	{
+		$this->buffer[] = [$level, $message, $context];
+	}
+
+	public function getBuffer(): array
+	{
+		return $this->buffer;
+	}
+}
