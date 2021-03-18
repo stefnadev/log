c8ac001122690d8e8405a4b56f28bc4f2c7084d3
Andreas Sundqvist <andreas@stefna.is>
2021-03-18 14:23:21 +0100

    Fix issue with CloudWatchFormatter on php7.4

--- src/Formatter/CloudWatchFormatter.php
+++ src/Formatter/CloudWatchFormatter.php
@@ -16,9 +16,19 @@ final class CloudWatchFormatter extends JsonFormatter
 		$record['datetime'] = $dateTime->format('Y-m-d\TH:i:s.up');
 		$line = parent::format($record);
 		if (isset($record['context']['requestId'])) {
+
+			$dateFormat = 'Y-m-d\TH:i:s.v';
+			if (PHP_VERSION_ID > 80000) {
+				$dateFormat .= 'p';
+			}
+			else {
+				// for php < 8 just hard code timezone to utc
+				$dateFormat .= '\Z';
+			}
+
 			$line = sprintf(
 				"%s\t%s\t%s\t%s",
-				$dateTime->format('Y-m-d\TH:i:s.vp'),
+				$dateTime->format($dateFormat),
 				$record['context']['requestId'],
 				$record['level_name'],
 				$line
