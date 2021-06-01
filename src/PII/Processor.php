e51a666c21ba059b287d6e5fd50339597419faf9
Andreas Sundqvist <andreas@stefna.is>
2021-06-01 15:52:44 +0200

    Add password anonymizer

--- src/PII/Processor.php
+++ src/PII/Processor.php
@@ -4,16 +4,19 @@ namespace Stefna\Logger\PII;
 
 use Stefna\Logger\PII\Anonymizer\Anonymizer;
 use Stefna\Logger\PII\Anonymizer\CardAnonymizer;
+use Stefna\Logger\PII\Anonymizer\PasswordAnonymizer;
 use Stefna\Logger\PII\Anonymizer\PersonAnonymizer;
 
 final class Processor
 {
+	/** @var Anonymizer[] */
 	private $anonymizers;
 
 	public function __construct(Anonymizer ...$anonymizers)
 	{
 		$this->anonymizers = $anonymizers;
 		if (!$this->anonymizers) {
+			$this->anonymizers[] = new PasswordAnonymizer();
 			$this->anonymizers[] = new CardAnonymizer();
 			$this->anonymizers[] = new PersonAnonymizer();
 		}
